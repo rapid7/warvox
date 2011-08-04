edfaf614409017aa10ff9507ca8c4498a5827879
HD Moore <hd_moore@rapid7.com>
2011-08-04 18:49:06 +0000

    Matches

--- web/app/models/dial_result.rb
+++ web/app/models/dial_result.rb
@@ -1,4 +1,21 @@
 class DialResult < ActiveRecord::Base
 	belongs_to :provider
 	belongs_to :dial_job
+	
+	
+	has_many :matches, :class_name => 'DialResult', :finder_sql =>
+		'SELECT dial_results.*,  ' +
+		'	(( icount(\'#{fprint}\'::int[] & dial_results.fprint::int[]) / icount(\'#{fprint}\'::int[])::float ) * 100.0 ) AS matchscore ' +
+		'FROM dial_results ' +
+		'WHERE icount(dial_results.fprint) > 0 AND ' + 
+		'dial_results.dial_job_id = \'#{dial_job_id}\' ' +
+		'ORDER BY matchscore DESC'
+
+	has_many :matches_all_jobs, :class_name => 'DialResult', :finder_sql =>
+		'SELECT dial_results.*,  ' +
+		'	(( icount(\'#{fprint}\'::int[] & dial_results.fprint::int[]) / icount(\'#{fprint}\'::int[])::float ) * 100.0 ) AS matchscore ' +
+		'FROM dial_results ' +
+		'WHERE icount(dial_results.fprint) > 0 ' +
+		'ORDER BY matchscore DESC'
+			
 end
