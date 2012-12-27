737268183d88f8b3d3182c020812a412e961ab4c
HD Moore <hd_moore@rapid7.com>
2012-12-27 15:28:43 -0600

    Permissions, ignorable

--- app/models/dial_result.rb
+++ app/models/dial_result.rb
@@ -5,7 +5,7 @@ class DialResult < ActiveRecord::Base
 
 	has_many :matches, :class_name => 'DialResult', :finder_sql => proc {
 		'SELECT dial_results.*,  ' +
-		"	(( icount(\'#{fprint}\'::int[] & dial_results.fprint::int[]) / icount(\'#{fprint}\'::int[])::float ) * 100.0 ) AS matchscore " +
+		"	(( icount(\'{#{fprint.map{|x| x.to_s}.join(",")}}\'::int[] & dial_results.fprint::int[]) / icount(\'{#{fprint.map{|x| x.to_s}.join(",")}}'::int[])::float ) * 100.0 ) AS matchscore " +
 		'FROM dial_results ' +
 		'WHERE icount(dial_results.fprint) > 0 AND ' +
 		"dial_results.dial_job_id = \'#{dial_job_id}\' AND " +
@@ -15,7 +15,7 @@ class DialResult < ActiveRecord::Base
 
 	has_many :matches_all_jobs, :class_name => 'DialResult', :finder_sql => proc {
 		'SELECT dial_results.*,  ' +
-		"	(( icount(\'#{fprint}\'::int[] & dial_results.fprint::int[]) / icount(\'#{fprint}\'::int[])::float ) * 100.0 ) AS matchscore " +
+		"	(( icount(\'{#{fprint.map{|x| x.to_s}.join(",")}}\'::int[] & dial_results.fprint::int[]) / icount(\'{#{fprint.map{|x| x.to_s}.join(",")}}\'::int[])::float ) * 100.0 ) AS matchscore " +
 		'FROM dial_results ' +
 		'WHERE icount(dial_results.fprint) > 0 AND ' +
 		"dial_results.id != \'#{id}\' " +
