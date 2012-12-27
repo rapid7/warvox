68961921fbd48f01b750b238e9494db37df271cc
HD Moore <hd_moore@rapid7.com>
2012-12-26 23:06:26 -0600

    Switch to config for all files

--- lib/warvox.rb
+++ lib/warvox.rb
@@ -10,8 +10,8 @@ require 'warvox/audio'
 
 # Global configuration
 module WarVOX
-	VERSION = '1.9.9-dev'
+	VERSION = '2.0.0-dev'
 	Base = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-	Conf = File.expand_path(File.join(Base, 'etc', 'warvox.conf'))
+	Conf = File.expand_path(File.join(Base, 'config', 'warvox.conf'))
 	JobManager = WarVOX::JobQueue.new
 end
