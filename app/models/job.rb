b2f5ee7c9079ae501f72a0c51d685dbc20983ec8
HD Moore <hd_moore@rapid7.com>
2013-01-07 12:24:40 -0600

    Cleanup

--- app/models/job.rb
+++ app/models/job.rb
@@ -107,7 +107,7 @@ class Job < ActiveRecord::Base
 				:scope      => self.scope,          # job / project/ global
 				:force      => !!(self.force),      # true / false
 				:target_id  => self.target_id.to_i, # job_id or project_id or nil
-				:target_ids => self.target_ids.map{|x| x.to_i }
+				:target_ids => (self.target_ids || []).map{|x| x.to_i }
 			})
 			return self.save
 		else
