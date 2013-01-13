47ccee3722153d696d6c0ceb4c202f504df4c3e6
HD Moore <hd_moore@rapid7.com>
2013-01-13 11:12:29 -0600

    Add a script for cleaning the Reportable cache

---
+++ bin/cache_clear.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+###################
+
+#
+# Load the library path
+#
+base = __FILE__
+while File.symlink?(base)
+	base = File.expand_path(File.readlink(base), File.dirname(base))
+end
+$:.unshift(File.join(File.expand_path(File.dirname(base)), '..', 'lib'))
+
+require 'warvox'
+require 'fileutils'
+require 'yaml'
+
+ENV['RAILS_ENV'] ||= 'production'
+$:.unshift(File.join(File.expand_path(File.dirname(base)), '..'))
+
+
+$stderr.puts "[*] Loading database environment..."
+
+require 'config/boot'
+require 'config/environment'
+
+
+$stderr.puts "[*] Clearing the report cache..."
+Saulabs::Reportable::ReportCache.delete_all
