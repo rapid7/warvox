a7ed1585a04b520a7a84acf517968df24010170c
zeknox <mccann.brandon@gmail.com>
2015-10-23 23:12:15 -0500

    fixed soft-tab indentation issues, also added initial project feature spec which tests listing of projects within projects_path view

---
+++ spec/features/projects_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "Projects", type: :feature do
+
+	before(:each) do
+		@user = create(:user)
+		create_user_session(@user)
+	end
+
+	it "list all existing projects" do
+    project = create(:project)
+		visit projects_path
+		expect(page).to have_content "WarVOX Projects"
+    within "#projects-table" do
+      expect(page).to have_content "Name"
+      expect(page).to have_content "Description"
+      expect(page).to have_content "Jobs"
+      expect(page).to have_content "Calls"
+      expect(page).to have_content "Analyzed"
+      expect(page).to have_content "Created"
+      expect(page).to have_content "Actions"
+      expect(page).to have_content project.name
+    end
+	end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,36 +20,36 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # FactoryGirl Syntax
-  config.include FactoryGirl::Syntax::Methods
+	# FactoryGirl Syntax
+	config.include FactoryGirl::Syntax::Methods
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+	# Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+	config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+	# If you're not using ActiveRecord, or you'd prefer not to run each of your
+	# examples within a transaction, remove the following line or assign false
+	# instead of true.
+	config.use_transactional_fixtures = true
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
-  config.infer_spec_type_from_file_location!
+	# RSpec Rails can automatically mix in different behaviours to your tests
+	# based on their file location, for example enabling you to call `get` and
+	# `post` in specs under `spec/controllers`.
+	#
+	# You can disable this behaviour by removing the line below, and instead
+	# explicitly tag your specs with their type, e.g.:
+	#
+	#     RSpec.describe UsersController, :type => :controller do
+	#       # ...
+	#     end
+	#
+	# The different available types are documented in the features, such as in
+	# https://relishapp.com/rspec/rspec-rails/docs
+	config.infer_spec_type_from_file_location!
 end
